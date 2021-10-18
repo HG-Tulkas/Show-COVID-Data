ba06d6e0a4327bb8ddd39d4ea5bfdb43cb00c2b2
Hauke <92751772+HG-Tulkas@users.noreply.github.com>
2021-10-18 20:30:05 +0200

    Update start batch file: use conda

--- covid_cnt.cmd
+++ covid_cnt.cmd
@@ -1,6 +1,9 @@
 cls
 echo COVID_CNT
 
-py COVID_19_DAN.py
+REM call  C:\Users\hauke\anaconda3\Scripts\activate.bat C:\Users\hauke\anaconda3
+call  C:\ProgramData\Miniconda3\Scripts\activate.bat C:\ProgramData\Miniconda3
+
+py -3.8-64 COVID_19_DAN.py
 
 pause
